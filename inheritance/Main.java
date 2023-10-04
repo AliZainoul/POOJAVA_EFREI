4f99cdc5e2e0692e24f431e66eaed68253bdb240
AliZainoul <ali.zainoul.az@gmail.com>
2023-10-04 14:55:59 +0200

    Final Version

--- inheritance/Main.java
+++ inheritance/Main.java
@@ -5,23 +5,40 @@ public class Main
   static void printLine() { System.out.println("----------------------------"); }
   public static void main(String[] args)
   {
+    System.out.println("-------------TEST CLASS A---------------");
     //ClassA ca = new ClassA(1, 2, 3, 4);
     ClassA ca = new ClassA(1, 2, 3, 4);
-    // printLine();
-    // System.out.println("Call to my privated Method:");
-    // ca.privateMethod();
-    System.out.println("-------------TEST CLASS A---------------");
+    
+    printLine();
+    System.out.println("Call to my public Method:");
+    ca.publicMethod();
+    System.out.println("Call to my public Member:" + ca.publicintA );
+    System.out.println("Call to my public Member with getter:" + ca.getpublicint());
+    printLine();
+
+    printLine();
+    System.out.println("Call to my default Method:");
+    ca.defaultMethod();
+    System.out.println("Call to my default Member:" + ca.defaultintA );
+    System.out.println("Call to my default Member with getter:" + ca.getdefaultint());
+    printLine();
+
+    printLine();
     System.out.println("Call to my protected Method:");
     ca.protectedMethod();
     System.out.println("Call to my protected Member:" + ca.protectedintA );
     System.out.println("Call to my protected Member with getter:" + ca.getprotectedint());
+    printLine();
+    
+    printLine();
+    System.out.println("Call to my private Method:");
+    ca.publicMethod(); // WHEREAS HERE IT WORKS, CAUSE THE CALL IS INDIRECT AND VIA A PUBLIC METHOD
+    ca.protectedMethod(); // WHEREAS HERE IT WORKS, CAUSE THE CALL IS INDIRECT AND VIA A PROCTED METHOD 
+    // ca.privateMethod(); // WILL CAUSE AN ERROR EVERY SINGLE TIME, CAUSE THE METHOD IS SIMPLY PRIVATE!
+    // System.out.println("Call to my private Member:" + ca.privateintA );
+    System.out.println("Call to my private Member with getter:" + ca.getprivateint());
+    printLine();
 
-    printLine();
-    System.out.println("Call to my public Method:");
-    ca.publicMethod();
-    printLine();
-    System.out.println("Call to my default Method:");
-    ca.defaultMethod();
     /*
     ClassB cb = new ClassB(1,2,3,4,5,6,7,8);
     System.out.println("-------------TEST CLASS B---------------");
